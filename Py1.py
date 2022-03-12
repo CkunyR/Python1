a3a35e9c11bdf6148b0d8c249b738b1bd9286a76
Zhenkun Guo <1608229093@qq.com>
2022-03-12 21:36:15 +0800

    入门1

---
+++ Py1.py
@@ -0,0 +1,14 @@
+#coding:gbk
+#name = 'abc'
+#print(name)
+#print('a',id(name))
+#print('b',type(name))
+#print('c',name)
+
+
+
+## 进制
+#print('10', 188)
+#print('2', 0b10110010)
+#print('8', 0o176)
+#print('16',0x1EC2)# .
